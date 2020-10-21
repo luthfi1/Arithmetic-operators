91f8949cfd00ad0f6a30b56fa68ecc76db94bee2
Muhammad Luthfi Farizqi Luthfi <luthfifarizqi23@gmail.com>
2020-10-21 01:51:53 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,13 @@
+//Arithmetic operators
+let data1 = 20
+let data2 = 13
+let data3 = 30
+
+let total = data1+data2
+print(total)
+let total1 = data1 - data2
+print(total1)
+let total2 = data1 * data2
+print(total2)
+let total3 = data1 / data2
+print(total3)
